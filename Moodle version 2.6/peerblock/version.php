576c8d3af1e9d434238be6df0e496fec08a21ade
jessicaribeiro <jessicafaaribeiro@gmail.com>
2016-05-16 22:21:36 +0100

    Remove versio 2.6

--- Moodle version 2.6/peerblock/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-$plugin->component = 'block_peerblock';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2016051001;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2013110500; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-
-$plugin->dependencies = array(
-    'mod_peerforum' => 2013110500,
-);
